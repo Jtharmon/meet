ff8cb6787c65ba5d89206c68dac295f5be31b4bd
Jtharmon <jtharmon10@gmail.com>
2023-06-12 16:12:37 -0400

    attempting to fix PWA service worker issue

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { InfoAlert } from './Alert';
 
 class NumberOfEvents extends Component {
@@ -6,36 +6,37 @@ class NumberOfEvents extends Component {
         super();
         this.state = {
             query: 32,
-            errorText: "",
+            errorText: '',
         };
     }
 
     handleInputChanged = (event) => {
         const value = event.target.value;
-        if (value >= 1 || value <= 32) {
+        if (value >= 1 && value <= 32) {
             this.setState({
                 query: value,
-                errorText: "",
+                errorText: '',
             });
-        }
-        if (value < 1 || value > 32) {
+        } else {
             this.setState({
                 query: value,
-                errorText: "Please enter a valid number",
+                errorText: 'Please enter a valid number',
             });
         }
     };
 
     render() {
         return (
-            <div className='NumberOfEvents'>
-                <input className='number-of-events-input'
-                    type='number'
+            <div className="NumberOfEvents">
+                <input
+                    className="number-of-events-input"
+                    type="number"
                     min={1}
                     max={32}
                     value={this.state.query}
                     onChange={this.handleInputChanged}
                 />
+                {this.state.errorText && <InfoAlert text={this.state.errorText} />}
             </div>
         );
     }
